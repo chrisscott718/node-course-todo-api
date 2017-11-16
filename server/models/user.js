2b91537322418da8c943e41187339eba6ba31162
Chris Scott <chris.scott718@gmail.com>
2017-11-16 15:38:46 -0800

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,19 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth'
   });
 };
+UserSchema.statics.findByCredentials = function(email, password){
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => res ? resolve(user) : reject());
+    });
+  });
+};
 
 // mongoose middleware
 UserSchema.pre('save', function(next){
